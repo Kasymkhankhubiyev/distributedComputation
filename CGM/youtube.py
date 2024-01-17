4c5f97d268f0cb8c4f3af86899c61c0e07106b2b
Kasymkhan <kasymkhankhubievnis@gmail.com>
2024-01-17 23:55:53 +0300

    started gradiend solution from youtube

--- CGM/youtube.py
+++ CGM/youtube.py
@@ -9,6 +9,12 @@ numprocs = comm.Get_size()
 rank = comm.Get_rank()
 
 
+def conjugate_gradient_method(A_part: np.ndarray, b_part: np.ndarray, 
+                              x_part: np.ndarray, N: int, N_part: int, 
+                              rcounts_N: np.ndarray, displs_N: np.ndarray) -> np.ndarray:
+    
+    pass
+
 if rank == 0:
     f1 = open('in.dat', 'r')
     N = np.array(f1.readline(), dtype=np.int32)
@@ -19,5 +25,11 @@ else:
 
 comm.Bcast([N, 1, MPI.DOUBLE], root=0)
 
-if rank == 0:
+def auxiallary_arrays(M: int, numprocs: int) -> 'tuple[np.ndarray, np.ndarray]':
     ave, res = divmod(M, numprocs-1)
+    rcounts = np.zeros(numprocs, dtype=np.int32)
+    displs = np.zeros(numprocs, dtype=np.int32)
+
+    for k in range(1, numprocs):
+        if k < 1 + res:
+            rcounts[k]
