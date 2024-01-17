02f195b708af3446680e65cf179058e45d2bfe66
Kasymkhan <kasymkhankhubievnis@gmail.com>
2024-01-17 18:14:48 +0300

    commit changes

--- CGM/parallel.py
+++ CGM/parallel.py
@@ -7,18 +7,72 @@ comm = MPI.COMM_WORLD
 numprocs = comm.Get_size()
 rank = comm.Get_rank()
 
-def сonjugate_gradient_method(A, b, x, N) :
+def сonjugate_gradient_method(A_part, b, x, N, M, M_part, N_part, comm,
+                              rcounts_M, rcounts_N, displs_M, displs_N):
     s = 1
-    p = np.zeros(N)
-    while s <= N :
-        if s == 1 :
-            r = np.dot(A.T, np.dot(A,x) - b)
-        else :
-            r = r - q/np.dot(p, q)
-        p = p + r/np.dot(r, r)
-        q = np.dot(A.T, np.dot(A, p))
-        x = x - p/np.dot(p,q)
+    p_part = np.zeros(N_part, dtype=np.float64)
+    while s <= N:
+        if s == 1:
+            # paralleling the following code:
+            # r = np.dot(A_part.T, np.dot(A_part,x) - b)
+
+            Ax_part = np.dot(A_part, x)
+            if rank == 0:
+                Ax = np.empty(M, dtype=np.float64)
+            else:
+                Ax = None
+            
+            comm.Gatherv([Ax_part, M_part, MPI.DOUBLE],
+                         [Ax, rcounts_M, displs_M, MPI.DOUBLE], root=0)
+            
+            if rank == 0:
+                Ax = Ax - b
+            
+            comm.Scatterv([Ax, rcounts_M, displs_M, MPI.DOUBLE],
+                          [Ax_part, M_part, MPI.DOUBLE], root=0)
+            
+            r_temp = np.dot(A_part.T, Ax_part)
+            r_part = np.empty(N_part, dtype=np.float64)
+            comm.Reduce_scatter([r_temp, N, MPI.DOUBLE],
+                                [r_part, N_part, MPI.DOUBLE],
+                                rcounts_N, MPI.SUM)
+        else:
+            # paralleling the following code:
+            # r = r - q/np.dot(p, q)
+
+            r_part = r_part - q_part / ScalP # np.dot(p_part, q_part)
+
+        # p = p + r/np.dot(r, r)
+        ScalP_temp = np.array(np.dot(r_part, r_part), dtype=np.float64)
+        ScalP = np.array(0, dtype=np.float64)
+        comm.Allreduce(ScalP_temp, ScalP, MPI.SUM)
+
+        p_part = p_part + r_part / ScalP
+
+        # paralleling the followinf code:
+        #q = np.dot(A_part.T, np.dot(A_part, p))
+        p = np.empty(N, dtype=np.float64)
+        comm.Allgatherv([p_part, N_part, MPI.DOUBLE],
+                     [p, rcounts_N, displs_N, MPI.DOUBLE])
+        
+        Ap_part = np.dot(A_part, p)
+        q_temp = np.dot(A_part.T, Ap_part)
+        q_part = np.empty(N_part, dtype=np.float64)
+        comm.Reduce_scatter([q_temp, N, MPI.DOUBLE],
+                            [q_part, N_part, MPI.DOUBLE],
+                            rcounts_N, MPI.SUM)
+
+        # paralleling the followinf code:
+        #x = x - p/np.dot(p,q)
+        ScalP_temp = np.array(np.dot(p_part, q_part), dtype=np.float64)
+        ScalP = np.array(0, dtype=np.float64)
+        comm.Allreduce(ScalP_temp, ScalP, MPI.SUM)
+        
+        if rank == 0:
+            x = x - p / ScalP
+
         s = s + 1
+
     return x
 
 if rank == 0:
@@ -29,11 +83,12 @@ if rank == 0:
     f1.close()
 else:
     N = np.array(0, dtype=np.int32)
-    M = np.array(0, dtype=np.int32)
+    M = None # np.array(0, dtype=np.int32)
 
-comm.Bcast(M, root=0)
+comm.Bcast(N, root=0)
 
-if rank == 0:
+
+def auxiallary_arrays(M, numprocs):
     rcounts = np.empty(numprocs, dtype=np.int32)
     displs = np.empty(numprocs, dtype=np.int32)
 
@@ -42,20 +97,29 @@ if rank == 0:
 
     ave, res = divmod(M, numprocs)
 
-    for k in range(0, numprocs):
+    for k in range(numprocs):
         if k < res:
             rcounts[k] = ave + 1
         else:
             rcounts[k] = ave
         displs[k] = displs[k-1] + rcounts[k-1]
 
-    print(rcounts, displs)
+    return rcounts, displs
+
+
+if rank == 0:
+    rcounts_M, displs_M = auxiallary_arrays(M, numprocs)
+    rcounts_N, displs_N = auxiallary_arrays(N, numprocs)
 else:
-    rcounts, displs = None, None
+    rcounts_M, displs_M = None, None
+    rcounts_N, displs_N = np.empty(numprocs, dtype=np.int32), None
 
 M_part = np.array(1, dtype=np.int32)
+N_part = np.array(1, dtype=np.int32)
 
-comm.Scatter([rcounts, 1, MPI.INT], [M_part, 1, MPI.INT], root=0)
+comm.Scatter([rcounts_M, 1, MPI.INT], [M_part, 1, MPI.INT], root=0)
+comm.Scatter([rcounts_N, 1, MPI.INT], [N_part, 1, MPI.INT], root=0)
+comm.Bcast(rcounts_N, root=0)
 
 if rank == 0:
     # Считываем из файла матрицу A
@@ -66,13 +130,13 @@ if rank == 0:
             A_part[j,i] = float(f2.readline())
 
     for k in range(1, numprocs):
-        A_part_temp = np.empty((rcounts[k] ,N), dtype=np.float64)
-        for j in range(rcounts[k]) :
+        A_part_temp = np.empty((rcounts_M[k] ,N), dtype=np.float64)
+        for j in range(rcounts_M[k]) :
             for i in range(N) :
                 A_part_temp[j,i] = float(f2.readline())
         comm.Send(A_part_temp, dest=k)
     f2.close()
-    del A_part_temp
+    # del A_part_temp
 else:
     A_part = np.empty((M_part, N), dtype=np.float64); 
     comm.Recv(A_part, source=0)
@@ -84,16 +148,18 @@ if rank == 0:
     for i in range(M):
         b[i] = float(f3.readline())
     f3.close()
+else:
+    b = None
 
-comm.Bcast(b, root=0)
+x = np.zeros(N, dtype=np.float64)
 
-x = np.zeros(N)
+x = сonjugate_gradient_method(A_part, b, x, N, M, M_part, N_part, comm,
+                              rcounts_M, rcounts_N, displs_M, displs_N)
 
-x = сonjugate_gradient_method(A_part, b, x, N)
+if rank == 0:
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0, N), ylim=(-1.5, 1.5))
+    ax.set_xlabel('i'); ax.set_ylabel('x[i]')
+    ax.plot(np.arange(N), x, '-r', lw=3)
 
-fig = plt.figure()
-ax = plt.axes(xlim=(0, N), ylim=(-1.5, 1.5))
-ax.set_xlabel('i'); ax.set_ylabel('x[i]')
-ax.plot(np.arange(N), x, '-r', lw=3)
-
-plt.show()
+    plt.show()
